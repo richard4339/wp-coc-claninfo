eaaffa935d889c7dfa43a22ac00e653648b1cb0e
Richard Lynskey <richard@mozor.net>
2016-07-02 11:06:01 -0500

    Initial

---
+++ wp-coc-claninfo.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: wp-coc-claninfo
+Plugin URI: https://github.com/richard4339/wp-coc-claninfo
+Description: A Wordpress plugin to add widgets for Clash of Clans info
+Version: 1.0
+Author: Richard Lynskey
+Author URI: https://github.com/richard4339/
+License: MIT
+*/
